290c55c5dce87c9eac33b1c7e8a1d8fc8957159b
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-14 16:47:02 +0530

    done

--- 2.CHAT_MODELS/test.py
+++ 2.CHAT_MODELS/test.py
@@ -4,3 +4,4 @@ model_id = "google/gemma-2-2b-it"
 resp = requests.get(f"https://huggingface.co/api/models/{model_id}?expand[]=inferenceProviderMapping")
 data = resp.json()
 print(data.get("inferenceProviderMapping"))
+#this was to check a model existing!
